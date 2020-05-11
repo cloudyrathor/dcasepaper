b776ef37b2c9f137b9f3011724158f09544ec893
cloudyrathor <bhushan.ugale@gmail.com>
2020-05-11 19:44:31 +0530

    model and data populator added_2

---
+++ populate_patients_data.py
@@ -0,0 +1,81 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','dcp_project.settings')
+import django
+django.setup()
+
+from dcp_api.models import pat_profile_table,med_profile_table,pat_treatment_history_table,treatment_plan_table
+
+
+
+from faker import Faker
+from random import *
+fake = Faker()
+# fake = Faker(['hi_IN'])
+
+def phonenumbergen():
+    p = randint(7,9)
+    pno = ''+str(p)
+    for i in range(9):
+        pno = pno + str(randint(0,9))
+    return str(pno)
+
+def populate(n):
+    for i in range(n):
+        pat_id = fake.random_int(min=1,max=999)
+        pat_fname = fake.name()
+        pat_dob = fake.date()
+        pat_address = fake.address()
+        pat_email = fake.address()
+        pat_mobile = phonenumbergen()
+        pat_aniversary = fake.date()
+        pat_habbit = fake.random_element(elements = ('Nail biting','Brushing hard','Grinding','chewing ice cube','constant snaking','using teeth as tool'))
+        pat_level_of_higine = fake.random_element(elements = ('Proper brushing','Flossing','Avoid Tobacco','Limit Sodas','Limit Coffee','Limit Alcohol','Consume Calsium','Regular Mouth wash'))
+        pat_cosmatic_concern = fake.random_element(elements = ('Teeth whitening','Bonding','Veneers','Crowns','Enamel Shaping and contouring','Braces','Bridges','Implant'))
+        pat_medical_history = fake.random_element(elements = ('Cardiovascular','Respiratory','Gastrointestinal','Hepatic','Neurological','Musculoskeletal','Drug history','Allergy'))
+        doc_id = fake.random_int(min=1,max=20)
+        doc_id_treated_by = doc_id
+        treatment_id = fake.random_int(min=1,max=99)
+        visit_date = fake.date()
+        observations = fake.random_element(elements = ('type1_observation','type2_observation','type3_observation','type4_observation','type5_observation'))
+        treatment_amount = fake.random_int(min=350,max=9999)
+        treatment_name = fake.random_element(elements = ('Teeth whitening','Bonding','Veneers','Crowns','Enamel Shaping and contouring','Braces','Bridges','Implant'))
+        # =====================pat_profile_table================================================================================================
+        pat_profile_table_data = pat_profile_table.objects.get_or_create(p_id = pat_id,
+                                                           p_name = pat_fname,
+                                                           p_dob = pat_dob,
+                                                           p_address = pat_address,
+                                                           p_email = pat_email,
+                                                           p_mobile = pat_mobile,
+                                                           p_aniversary = pat_aniversary)
+        # =======================================================================================================================
+
+        # =====================med_profile_table=================================================================================
+        med_profile_table_data = med_profile_table.objects.get_or_create(p_id=pat_id,
+                                                                         p_habbit=pat_habbit,
+                                                                         p_level_of_higine=pat_level_of_higine,
+                                                                         p_cosmatic_concern=pat_cosmatic_concern,
+                                                                         p_medical_history=pat_medical_history)
+
+        # =======================================================================================================================
+
+        # =====================pat_treatment_history_table================================================================================================
+        pat_treatment_history_table_data = pat_treatment_history_table.objects.get_or_create(p_id=pat_id,
+                                                                         d_id_treated_by=doc_id_treated_by,
+                                                                         treatment_id=treatment_id,
+                                                                         visit_date=visit_date,
+                                                                         observations=observations,
+                                                                         treatment_amount=treatment_amount)
+        # =======================================================================================================================
+
+        # =====================treatment_plan_table================================================================================================
+        treatment_plan_table_data = treatment_plan_table.objects.get_or_create(t_id=treatment_id,
+                                                                         d_id=doc_id,
+                                                                         treatment_name=treatment_name,
+                                                                         treatment_amount=treatment_amount)
+        # =======================================================================================================================
+
+
+populate(100)
+
+
+
