6fd0fc1d8dfbc3f4c07ed95d521b28b77d69affe
mrimmortal <akshayvdani@gmail.com>
2020-05-14 17:13:23 +0530

    View.py : 1. PostPatients (get (List) & Post(Create one Record) ) 2. PostPatient ( Single get & Single Delete ) Lookup_field = 'p_id'
    urls.py:
    Two urlpaths added
    1. patients/
    2. patients/int p_id

--- dcp_api/urls.py
+++ dcp_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from dcp_api.views import patientListView
+from dcp_api.views import *
 
 urlpatterns = [
-    path('',patientListView.as_view(), name='list'),
+    path('patients/',PostPatients.as_view(), name='list'),   
+    path('patients/<int:p_id>/',PostPatient.as_view(), name ='member' ),
+    #path('articles/<int:year>/<int:month>/', views.month_archive),
+    #path('articles/<int:year>/<int:emonth>/<slug:slug>/', views.article_detail),
 ]
--- dcp_api/views.py
+++ dcp_api/views.py
@@ -3,12 +3,36 @@ from dcp_api.models import *
 from dcp_api.Serializers import *
 from rest_framework import status
 from rest_framework import generics 
-from rest_framework.mixins import ListModelMixin
+from rest_framework import mixins
 
 
-class patientListView(generics.GenericAPIView,ListModelMixin):
+class PostPatients(mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     generics.GenericAPIView):
+
     serializer_class = pat_profile_tableSerializer
     queryset = pat_profile_table.objects.all()
+        
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class PostPatient(mixins.RetrieveModelMixin,
+                mixins.DestroyModelMixin,
+                generics.GenericAPIView):
+
+    queryset = pat_profile_table.objects.all()
+    serializer_class = pat_profile_tableSerializer
+    lookup_field = 'p_id'
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)        
+
 
 # Create your views here test.
 
