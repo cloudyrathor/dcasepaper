5af4c460634cf5228a659a1a1fe4ee6f99a278d7
cloudyrathor <bhushan.ugale@gmail.com>
2020-05-15 18:53:30 +0530

    commit after url change in app

--- dcp_api/urls.py
+++ dcp_api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from dcp_api.views import *
 
 urlpatterns = [
-    path('patients/',PostPatients.as_view(), name='list'),   
-    path('patients/<int:p_id>/',PostPatient.as_view(), name ='member' ),
+    path('patient_list/',PostPatients.as_view(), name='list'),   
+    path('patient/<int:p_id>/',PostPatient.as_view(), name ='member' ),
     #path('articles/<int:year>/<int:month>/', views.month_archive),
     #path('articles/<int:year>/<int:emonth>/<slug:slug>/', views.article_detail),
 ]
