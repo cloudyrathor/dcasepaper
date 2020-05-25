d534b2e0591524d5ecbdc4d815dc15e21f6985bc
cloudyrathor <bhushan.ugale@gmail.com>
2020-05-25 16:06:09 +0530

    Model and populator updated

--- dcp_api/models.py
+++ dcp_api/models.py
@@ -9,8 +9,7 @@ class pat_profile_table(models.Model):
     p_email = models.EmailField()
     p_mobile = models.CharField(max_length=10)
     p_anniversary = models.DateField()
-    class Meta:
-      managed = False
+
 
 class med_profile_table(models.Model):
     p_id = models.ForeignKey(pat_profile_table ,on_delete=models.CASCADE) 
@@ -18,11 +17,10 @@ class med_profile_table(models.Model):
     p_level_of_higgins = models.CharField(max_length=64)
     p_cosmetic_concern = models.CharField(max_length=128)
     p_medical_history = models.CharField(max_length=1024)
-    class Meta:
-      managed = False
+
 
 class pat_treatment_history_table(models.Model):
-    p_id = models.IntegerField()
+    p_id = models.ForeignKey(pat_profile_table ,on_delete=models.CASCADE)
     d_id_treated_by = models.CharField(max_length=64)
     treatment_id = models.IntegerField()
     visit_date = models.DateField()
